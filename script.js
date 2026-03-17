21c0ccdcfe3fcf612f492f0c94f15718c2cf3061
jfarfanp <john.farfan2520@gmail.com>
2026-03-16 21:36:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+(function(){
+emailjs.init("TU_PUBLIC_KEY");
+})();
+
 document.getElementById("formulario").addEventListener("submit", function(e){
+e.preventDefault();
 
-    e.preventDefault();
-
-    alert("Mensaje enviado correctamente");
-
+emailjs.send("TU_SERVICE_ID","TU_TEMPLATE_ID",{
+name: document.querySelector("input[type=text]").value,
+email: document.querySelector("input[type=email]").value,
+message: document.querySelector("textarea").value
+})
+.then(function(){
+alert("Correo enviado correctamente");
+});
 });
